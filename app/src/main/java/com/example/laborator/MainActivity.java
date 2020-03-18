c44cd99d7a049cc1ae968e1e487de7af51e81087
Cosmin Popescu <popescucosminc@gmail.com>
2020-03-18 19:28:26 +0200

    Laborator 4

--- app/src/main/java/com/example/laborator/MainActivity.java
+++ app/src/main/java/com/example/laborator/MainActivity.java
@@ -1,23 +1,28 @@
 package com.example.laborator;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.laborator.MESSAGE";
     private ListView mainListView;
     Toolbar toolbar;
 
@@ -31,15 +36,16 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         mainListView = findViewById(R.id.mainListView);
 
-        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.ProductName));
 
         mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("ProductName", mainListView.getItemAtPosition(position).toString());
-                startActivity(intent);
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, mainListView.getItemAtPosition(position).toString());
+                startActivity(Intent.createChooser(intent,"Choose app:"));
             }
         });
         mainListView.setAdapter(mAdapter);
@@ -90,21 +96,92 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.item1:
-                Toast.makeText(this, "Edit option selected", Toast.LENGTH_SHORT).show();
+            case R.id.item1: {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Do you want to change the title?");
+
+                final EditText newTitle = new EditText(MainActivity.this);
+                newTitle.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(newTitle);
+
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        toolbar = findViewById(R.id.toolbar);
+                        toolbar.setTitle(newTitle.getText().toString());
+                        Toast.makeText(MainActivity.this, "Title changed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
                 return true;
-            case R.id.item2:
-                Toast.makeText(this, "Settings option selected", Toast.LENGTH_SHORT).show();
+            }
+            case R.id.item2: {
+                //settings
+                AlertDialog.Builder info = new AlertDialog.Builder(MainActivity.this);
+                String message = "This app was created by Cosmin Popescu and it is intended to be a school laboratory assignment.";
+                info.setTitle("Informations");
+                info.setMessage(message);
+                info.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                info.show();
                 return true;
-            case R.id.item3:
-                Toast.makeText(this, "More option selected", Toast.LENGTH_SHORT).show();
+            }
+            case R.id.item3: {
+                //more
                 return true;
-            case R.id.subitem1:
-                Toast.makeText(this, "Rate app option selected", Toast.LENGTH_SHORT).show();
+            }
+            case R.id.subitem1:{
+//                Toast.makeText(this, "Rate app option selected", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Do you enjoy our app?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AlertDialog.Builder buttonYes = new AlertDialog.Builder(MainActivity.this);
+                        buttonYes.setTitle("Rate yes");
+                        buttonYes.setMessage("Thank you for your feedback!");
+                        buttonYes.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Toast.makeText(MainActivity.this, "Thank you", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                                buttonYes.show();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AlertDialog.Builder buttonNo = new AlertDialog.Builder(MainActivity.this);
+                        buttonNo.setTitle("Rate No");
+                        buttonNo.setMessage("We are sorry to hear that. We'll still work to make your experience better.");
+                        buttonNo.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                        buttonNo.show();
+                    }
+                });
+                builder.show();
                 return true;
-            case R.id.subitem2:
-                Toast.makeText(this, "Exit app option selected", Toast.LENGTH_SHORT).show();
+            }
+            case R.id.subitem2: {
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
                 return true;
+            }
             default:
                 return super.onOptionsItemSelected(item);
         }
--- app/src/main/java/com/example/laborator/SecondActivity.java
+++ app/src/main/java/com/example/laborator/SecondActivity.java
@@ -1,55 +1,94 @@
 package com.example.laborator;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
     Toolbar mToolbar;
     TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        textView = (TextView) findViewById(R.id.detailTextView);
+        mToolbar = findViewById(R.id.toolbar);
+        textView = findViewById(R.id.detailTextView);
+        button = findViewById(R.id.button);
 
         Bundle bundle = getIntent().getExtras();
-        if (bundle != null) {
-            mToolbar.setTitle(bundle.getString("ProductName"));
-            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_1")) {
-                textView.setText(bundle.getString("ProductName"));
-            }
-            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_2")) {
-                textView.setText(bundle.getString("ProductName"));
-            }
-            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_3")) {
-                textView.setText(bundle.getString("ProductName"));
-            }
-            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_4")) {
-                textView.setText(bundle.getString("ProductName"));
-            }
-            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_5")) {
-                textView.setText(bundle.getString("ProductName"));
-            }
-            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_6")) {
-                textView.setText(bundle.getString("ProductName"));
-            }
-            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_7")) {
-                textView.setText(bundle.getString("ProductName"));
-            }
-            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_8")) {
-                textView.setText(bundle.getString("ProductName"));
-            }
+        if (bundle != null)
+        {
+            String message = bundle.getString(Intent.EXTRA_TEXT);
+            textView.setText(message);
         }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SecondActivity.this);
+                builder.setTitle("Help")
+                        .setMessage("Was this helpful?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                builder.show();
+            }
+        });
+
+
+
+        /// the below is doing the same thing as the above 3 lines but a lot more uglier
+//        if (bundle != null) {
+//            mToolbar.setTitle(bundle.getString("ProductName"));
+//            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_1")) {
+//                textView.setText(bundle.getString(Intent.EXTRA_TEXT));
+//            }
+//            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_2")) {
+//                textView.setText(bundle.getString(Intent.EXTRA_TEXT));
+//            }
+//            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_3")) {
+//                textView.setText(bundle.getString(Intent.EXTRA_TEXT));
+//            }
+//            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_4")) {
+//                textView.setText(bundle.getString(Intent.EXTRA_TEXT));
+//            }
+//            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_5")) {
+//                textView.setText(bundle.getString(Intent.EXTRA_TEXT));
+//            }
+//            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_6")) {
+//                textView.setText(bundle.getString(Intent.EXTRA_TEXT));
+//            }
+//            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_7")) {
+//                textView.setText(bundle.getString(Intent.EXTRA_TEXT));
+//            }
+//            if (mToolbar.getTitle().toString().equalsIgnoreCase("Product_8")) {
+//                textView.setText(bundle.getString(Intent.EXTRA_TEXT));
+//            }
+//        }
     }
 
     @Override
